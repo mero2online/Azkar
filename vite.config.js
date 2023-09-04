5c13c7de741b6b205828b78bbd072067587d1126
mero2online <mero2online@gmail.com>
2023-09-05 01:12:32 +0300

    ADD: base and port ti vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'zekr',
+  server: {
+    port: '3000',
+    // hmr: { overlay: false },
+  },
   plugins: [react()],
-})
+});
