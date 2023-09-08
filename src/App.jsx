54cc66200668c3088477a8faf1c3310a750fb823
mero2online <mero2online@gmail.com>
2023-09-08 06:26:40 +0300

    ADD: New Dua

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import './App.css';
 import ZekrGUI from './ZekrGUI';
 
 const Azkar = [
+  { id: 97, name: 'دعاء السفر' },
   { id: 28, name: 'أذكار المساء' },
   { id: 27, name: 'أذكار الصباح' },
   { id: 25, name: 'الأذكار بعد الصلاة' },
 ];
+
 function App() {
   const [current, setCurrent] = useState(null);
   return (
